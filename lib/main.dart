37dc0f8c74a0addd1d812dbdeaac037da124904b
amornpan phornchaicharoen <amornpan@gmail.com>
2022-08-22 17:30:35 +0700

    dartpad.txt and main.dart first day

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -50,8 +50,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  String fullname = 'Amornpan Phornchaicharoen';
+
+  Map<String, Object> product = {
+    'id': 10,
+    'name': 'Coke',
+  };
+
   void _incrementCounter() {
     setState(() {
+      print('## debug');
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
@@ -94,19 +102,25 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
+
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this many times: ',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 31.6,
+              ),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Name: $fullname\nProduct Id: ${product['id']}\nProduct Name: ${product['name']}',
+              style: Theme.of(context).textTheme.headline5,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
+        backgroundColor: Colors.amber,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
