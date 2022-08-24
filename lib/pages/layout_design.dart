3cfd49f3bd7c160b8b3e0ab3b28a80a6dc51c519
amornpan phornchaicharoen <amornpan@gmail.com>
2022-08-24 10:08:43 +0700

    Layout page design 2

--- lib/pages/layout_design.dart
+++ lib/pages/layout_design.dart
@@ -21,16 +21,49 @@ class _LayoutDesignState extends State<LayoutDesign> {
             children: <Widget>[
               Image.asset('assets/images/building-2.png'),
               Padding(
-                padding: const EdgeInsets.all(15),
+                padding:  EdgeInsets.all(15),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
+                  children:  [
+                    const Text(
                       'R&D National Telecom',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                      style:  TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                      ),
                     ),
-                    Divider(),
+                    const Divider(),
+                    const Text(
+                      'บริษัท โทรคมนาคมแห่งชาติ จำกัด (มหาชน) หรือ NT โดย นายวิรัตน์ บุญมี ผู้จัดการส่วนบริหารจัดการทรัพย์สินและการใช้พื้นที่ NT ผู้ประพันธ์เพลง“คุณธรรม คุณทำได้” ในกิจกรรมส่งเสริมค่านิยม วินัย พอเพียง สุจริต จิตอาสา ของ NT ประจำปี 2564 คว้า “รางวัลชนะเลิศ รางวัลคุณธรรม อวอร์ด ประเภทสื่อ สาขาบทเพลง ปี 2564” ',
+                    ),
+                    const Divider(),
+                    Row(
+                      children: [
+                        const Expanded(
+                          child: Icon(
+                            Icons.pin_drop_outlined,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                        Expanded(
+                            flex: 6,
+                            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const[
+                                Text(
+                                  'admin@nt.ntplc.co.th',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blueGrey,
+                                  ),
+                                ),
+                                Text(
+                                  'บริษัท โทรคมนาคมแห่งชาติ จำกัด (มหาชน) 99 ถนนแจ้งวัฒนะ แขวงทุ่งสองห้อง เขตหลักสี่ กรุงเทพ 10210',
+                                ),
+                              ],
+                            )),
+                        //Expanded(child: child),
+                      ],
+                    ),
                   ],
                 ),
               ),
