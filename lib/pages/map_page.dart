e94089eb4bb52d4be3a1b1cd1f5b3f319a87a941
amornpan phornchaicharoen <amornpan@gmail.com>
2022-08-26 13:53:38 +0700

    Google map 1

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../widgets/menu_drawer.dart';
 
@@ -10,12 +13,27 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _kNTRandD = CameraPosition(
+    target: LatLng(14.02100653681219, 100.52788269999999),
+    zoom: 16.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const MenuDrawer(),
       appBar: AppBar(
         title: const Text('Map Page'),
+      ),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kNTRandD,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
