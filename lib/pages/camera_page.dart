cebd34bc527ec9c8d8c6379b91b96a8b9715e75c
amornpan phornchaicharoen <amornpan@gmail.com>
2022-08-26 14:39:20 +0700

    Take Photo completed

--- lib/pages/camera_page.dart
+++ lib/pages/camera_page.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import '../widgets/menu_drawer.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
@@ -10,12 +13,49 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
+  final ImagePicker _picker = ImagePicker();
+
+  XFile? _imageFile;
+
+  Future<void> takePhoto() async {
+    final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _imageFile = photo;
+    });
+  }
+
+  Widget previewImage() {
+    if (_imageFile != null) {
+      return Center(
+        child: Image.file(
+          File(_imageFile!.path),
+          fit: BoxFit.fill,
+          width: double.infinity,
+          alignment: Alignment.center,
+        ),
+      );
+    } else {
+      //CircularProgressIndicator()
+      return const Center(
+        child: Text('กรุณากดปุ่มด่านล่างเพื่อถ่ายรูป'),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const MenuDrawer(),
       appBar: AppBar(
         title: const Text('Camera Page'),
+      ),
+      body: previewImage(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          takePhoto();
+        },
+        tooltip: 'ถ่ายรูป',
+        child: const Icon(Icons.camera_alt),
       ),
     );
   }
