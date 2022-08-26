2a387485e12dadd52a053fb647f66dce3fba4440
amornpan phornchaicharoen <amornpan@gmail.com>
2022-08-26 15:22:12 +0700

    Finish course

--- lib/pages/camera_page.dart
+++ lib/pages/camera_page.dart
@@ -1,9 +1,13 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../widgets/menu_drawer.dart';
 import 'package:image_picker/image_picker.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
@@ -48,6 +52,45 @@ class _CameraPageState extends State<CameraPage> {
       drawer: const MenuDrawer(),
       appBar: AppBar(
         title: const Text('Camera Page'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              if (_imageFile != null) {
+                //convert jpg to base64
+                final bytesImage = await File(_imageFile!.path).readAsBytes();
+                var base64Image = base64Encode(bytesImage);
+
+                //upload to server
+                var url =
+                    Uri.parse('https://api.codingthailand.com/api/upload');
+                var response = await http.post(
+                  url,
+                  headers: {'Content-Type': 'application/json'},
+                  body: json.encode(
+                    {
+                      'picture': 'data:image/jpeg;base64,$base64Image',
+                    },
+                  ),
+                );
+                if (response.statusCode == 200) {
+                  var responseMessage = json.decode(response.body);
+                  Get.snackbar(
+                    '${responseMessage['data']['message']}',
+                    '${responseMessage['data']['url']}',
+                    backgroundColor: Colors.greenAccent,
+                  );
+
+                  setState(() {
+                    _imageFile = null;
+                  });
+                }
+              }
+            },
+            icon: const Icon(
+              Icons.upload_file,
+            ),
+          )
+        ],
       ),
       body: previewImage(),
       floatingActionButton: FloatingActionButton(
